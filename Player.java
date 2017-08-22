a4c26717c9713141d3d90d452bfc209cc7912f24
ThomasSzewczyk <30422372+ThomasSzewczyk@users.noreply.github.com>
2017-08-21 21:35:51 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,74 +1,67 @@
 public class Player extends StockGame
-{
-	private double cash;
-	
-	
-	public Player()
 	{
-		cash=500;
-	}
-	
-	public void buyStocks(int numstocks,String stockName)
-	{
-		if(stockName.equals("Yahoo"))
+		private int cash;
+		
+		
+		public Player()
 		{
-		cash-=numstocks*getYahooPrice();
-		setYahooPrice((1+(.02*numstocks))*getYahooPrice());
-		}
-		else if(stockName.equals("Google"))
-		{
-			cash-=numstocks*getGooglePrice();
-			setGooglePrice((1+(.02*numstocks))*getGooglePrice());
-		}
-		else if(stockName.equals("Apple"))
-		{
-			cash-=numstocks*getApplePrice();
-			setApplePrice((1+(.02*numstocks))*getApplePrice());
-		}
-		else if(stockName.equals("Disney"))
-		{
-			cash-=numstocks*getDisneyPrice();
-			setDisneyPrice((1+(.02*numstocks))*getDisneyPrice());
-		}
-		else
-		{
-			cash=cash;
-		}
-	}
-	
-		public void sellStocks(int numStocks,String stockName)
-		{
-		if(stockName.equals("Yahoo"))
-		{
-		cash+=numStocks*getYahooPrice();
-		setYahooPrice((1-(.02*numStocks))*getYahooPrice());
-		}
-		else if(stockName.equals("Google"))
-		{
-			cash+=numStocks*getGooglePrice();
-			setGooglePrice((1-(.02*numStocks))*getGooglePrice());
-		}
-		else if(stockName.equals("Apple"))
-		{
-			cash+=numStocks*getApplePrice();
-			setApplePrice((1-(.02*numStocks))*getApplePrice());
-		}
-		else if(stockName.equals("Disney"))
-		{
-			cash+=numStocks*getDisneyPrice();
-			setDisneyPrice((1-(.02*numStocks))*getDisneyPrice());
-		}
-		else
-		{
-			cash=0000000;
-		}
+			cash=50;
 		}
 		
-		public double getCash()
+		public void buyStocks(int numstocks,String stockName)
 		{
-		return cash;
+			if(stockName.equals("Yahoo"))
+			{
+			cash=cash-(numstocks*getYahooPrice());// The problme is right here with the calling of getYahooPrice which calls a bad value. Player and computer can extend StocKGame because in Runner they have differnt intitialized Objects.class Must find a way to call Yahoo price
+			
+			}
+			else if(stockName.equals("Google"))
+			{
+				cash-=(numstocks*getGooglePrice());
+				
+			}
+			else if(stockName.equals("Apple"))
+			{
+				cash-=(numstocks*getApplePrice());
+				
+			}
+			else if(stockName.equals("Disney"))
+			{
+				cash-=(numstocks*getDisneyPrice());
+				
+			}
+			
 		}
 		
-}
- 
- 
+			public void sellStocks(int numStocks,String stockName)
+			{
+			if(stockName.equals("Yahoo"))
+			{
+			cash+=(numStocks*getYahooPrice());
+			
+			}
+			else if(stockName.equals("Google"))
+			{
+				cash+=(numStocks*getGooglePrice());
+				
+			}
+			else if(stockName.equals("Apple"))
+			{
+				cash+=(numStocks*getApplePrice());
+				
+			}
+			else if(stockName.equals("Disney"))
+			{
+				cash+=(numStocks*getDisneyPrice());
+				
+			}
+			}
+			
+			public double getCash()
+			{
+			return cash;
+			}
+			
+	}
+
+
